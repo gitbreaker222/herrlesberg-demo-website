7fd98f8ea8429e5012a78aa12434d558d59c5b9f
Ruben L <ruben.labiunda@openmailbox.org>
2017-08-28 17:26:42 +0200

    add files from pages and sort by position (error)

--- dato.config.js
+++ dato.config.js
@@ -28,10 +28,30 @@ module.exports = (dato, root, i18n) => {
   })
 
   /*
-  === POSTS ===
+  === PAGES ===
   */
-  root.directory("src/content/posts", (folder) => {
-    dato.posts.forEach((post) => {
+  root.directory("src/content", (folder) => {
+    console.log(dato.pages)
+    dato.pages.forEach((item) => {
+      folder.createPost(
+        `${item.slug}.md`, "yaml", {
+          frontmatter: {
+            layout: 'layout',
+            title: item.title,
+            position: item.position,
+            image: item.image,
+          },
+          content: item.content
+        }
+      )
+    })
+  })
+
+  /*
+  === ARTICLES ===
+  */
+  root.directory("src/content/articles/", (folder) => {
+    dato.articles.forEach((post) => {
       folder.createPost(
         `${post.slug}.md`, "yaml", {
           frontmatter: {
@@ -47,63 +67,3 @@ module.exports = (dato, root, i18n) => {
   })
 
 }
-/*
-
-
-module.exports = (dato, root, i18n) => {
-
-  // Create a YAML data file to store global data about the site
-  root.createDataFile('src/data/settings.yml', 'yaml', {
-    name: dato.site.globalSeo.siteName,
-    language: dato.site.locales[0],
-    intro: dato.home.introText,
-    copyright: dato.home.copyright,
-    // iterate over all the `social_profile` item types
-    socialProfiles: dato.socialProfiles.map(profile => {
-      return {
-        type: profile.profileType.toLowerCase().replace(/ +/, '-'),
-        url: profile.url,
-      }
-    }),
-    faviconMetaTags: dato.site.faviconMetaTags,
-    seoMetaTags: dato.home.seoMetaTags
-  })
-
-  // Create a markdown file with content coming from the `about_page` item
-  // type stored in DatoCMS
-  root.createPost(`src/about.md`, 'yaml', {
-    frontmatter: {
-      title: dato.aboutPage.title,
-      subtitle: dato.aboutPage.subtitle,
-      photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
-      layout: 'about.ejs',
-      seoMetaTags: dato.aboutPage.seoMetaTags,
-    },
-    content: dato.aboutPage.bio
-  })
-
-  // Create a `works` directory (or empty it if already exists)...
-  root.directory('src/works', dir => {
-    // ...and for each of the works stored online...
-    dato.works.forEach((work, index) => {
-      // ...create a markdown file with all the metadata in the frontmatter
-      dir.createPost(`${work.slug}.md`, 'yaml', {
-        frontmatter: {
-          title: work.title,
-          coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
-          detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
-          position: index,
-          excerpt: work.excerpt,
-          seoMetaTags: work.seoMetaTags,
-          extraImages: work.gallery.map(image =>
-            image.url({ h: 300, fm: 'jpg', auto: 'compress' })
-          ),
-        },
-        content: work.description
-      })
-    })
-  })
-}
-
-
-*/
